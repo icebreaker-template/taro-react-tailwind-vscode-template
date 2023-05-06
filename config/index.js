7460bef2be3a3dab79f8623800c78c9eb9caed1e
SonOfMagic <qq1324318532@gmail.com>
2023-05-06 11:23:43 +0800

    chore: codes

--- config/index.js
+++ config/index.js
@@ -23,7 +23,7 @@ const config = {
   framework: "react",
   compiler: "webpack5",
   cache: {
-    enable: false // Webpack 持久化缓存配置，建议开启。默认配置请参考：https://docs.taro.zone/docs/config-detail#cache
+    enable: true // Webpack 持久化缓存配置，建议开启。默认配置请参考：https://docs.taro.zone/docs/config-detail#cache
   },
   mini: {
     postcss: {
