6ac59670f9b876139de6beadac5fb5e48fce1d83
SonOfMagic <qq1324318532@gmail.com>
2023-05-06 15:44:58 +0800

    feat: commit codes

--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -1,35 +1,42 @@
 import { useState } from "react";
 import { View } from "@tarojs/components";
-
+import { clsx } from "clsx";
 import "./index.scss";
 
 const Index = () => {
-  const [flag] = useState(true);
-  const className = "bg-[#123456]";
+  const [flag, setFlag] = useState(true);
+  const className = clsx(flag ? "bg-[#123456]" : "bg-[#654321]", "text-white");
+  const logoClass = clsx(
+    "bg-[url(https://pic1.zhimg.com/v2-3ee20468f54bbfefcd0027283b21aaa8_720w.jpg)] bg-[length:100%_100%] bg-no-repeat w-screen h-[41.54vw]"
+  );
   return (
     <>
-      <View className='[&_.u-count-down\_\_text]:!text-red-400'>
-        <View>1</View>
-        <View>2</View>
-        <View></View>
+      <View className={logoClass}></View>
+      <View className='[&_.u-count-down\_\_text]:!text-sky-400'>
         <View></View>
         <View>
-          <View className='u-count-down__text'>taro-react-tailwind-vscode-template</View>
+          <View className='u-count-down__text text-[50px] text-center'>
+            taro-react-tailwind-vscode-template
+          </View>
         </View>
       </View>
-      <View className="after:content-['presudo'] after:ml-0.5 after:text-red-500"></View>
-      <View className="after:content-['我是伪元素'] after:ml-0.5 after:text-red-500"></View>
+      <View className='space-y-4'>
+        <View className="after:content-['hello_taro!'] after:ml-0.5 after:text-lime-700"></View>
+        <View
+          className="bg-gray-100 dark:bg-zinc-800 h-20 w-40 after:text-xs after:content-['this_is_a_hover_block.have_a_try!']"
+          hoverClass='bg-red-500 dark:bg-green-500'
+        ></View>
 
-      <View
-        className='bg-gray-100 dark:bg-zinc-800 h-10 w-10'
-        hoverClass='bg-red-500 dark:bg-green-500'
-      ></View>
-
-      <View className={className}>className</View>
-      <View className={flag ? "p-[20px] -mt-2 mb-[-20px] " : ""}>
-        1111
+        <View
+          className={className}
+          onClick={() => {
+            setFlag(!flag);
+          }}
+        >
+          click to switch className
+        </View>
+        <View className='test'>@apply classes</View>
       </View>
-      <View className='test'>test</View>
     </>
   );
 };
