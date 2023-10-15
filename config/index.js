bf9cf7a94bce9a23d0d6788d0b2de795a304392e
SonOfMagic <qq1324318532@gmail.com>
2023-10-15 20:50:30 +0800

    chore(deps): upgrade and disable prebundle

--- config/index.js
+++ config/index.js
@@ -25,7 +25,12 @@ const config = {
     }
   },
   framework: 'react',
-  compiler: 'webpack5',
+  compiler: {
+    type: 'webpack5',
+    prebundle: {
+      enable: false,
+    }
+  },
   cache: {
     enable: false // Webpack 持久化缓存配置，建议开启。默认配置请参考：https://docs.taro.zone/docs/config-detail#cache
   },
