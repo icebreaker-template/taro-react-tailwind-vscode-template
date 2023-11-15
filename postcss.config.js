6446b1785a9aa8f50268747a50cc5a613005911f
ice breaker <1324318532@qq.com>
2023-11-15 17:05:12 +0800

    chore(deps): upgrade

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,16 @@
 module.exports = {
   plugins: {
-    tailwindcss: {}
+    tailwindcss: {},
     // autoprefixer: {}
+    'postcss-rem-to-responsive-pixel': {
+      // 32 意味着 1rem = 32rpx
+      rootValue: 32,
+      // 默认所有属性都转化
+      propList: ['*'],
+      // 转化的单位,可以变成 px / rpx
+      transformUnit: 'rpx',
+      // postcss-rem-to-responsive-pixel@6 版本添加了 disabled 参数，用来禁止插件的转化
+      disabled: process.env.TARO_ENV === 'h5' || process.env.TARO_ENV === 'rn'
+    },
   }
 };
